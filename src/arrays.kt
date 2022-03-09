0f93463db6a5cafc67a9912bfff2e5480c6a6e70
bakhitaismail <bakhitaismailk@gmail.com>
2022-03-09 16:51:30 +0300

    third commit

--- src/arrays.kt
+++ src/arrays.kt
@@ -1,31 +1,34 @@
 fun main() {
-    val names = arrayOf("Bakhita", "Aisha", "Bella", "Naima")
-    println(names.contentToString())
-    capitalCities()
-    maths()
-    println(secondNames().contentToString())
+    array()
+    city()
+    add()
+var s = names(arrayOf("secia","serd","dell"))
+    println(s.contentToString())
+
 
 }
-fun capitalCities() {
-    var cities = arrayOf("harare", "mumbai", "dodoma", "jakarta")
-    cities.forEach { x ->
+fun array(){
+    var x =arrayOf("speria","sper","tanya","win")
+    println(x.contentToString())
+}
+fun city(){
+    var cities=arrayOf("harare","mumbai","dodoma","jakarta")
+    cities.sortedArray().forEach { x->
         println(x.capitalize())
-
     }
-
 }
-fun maths() {
-    var numbers = arrayOf(32, 17, 4, 213, 78, 43, 90, 31, 3, 73, 11, 158, 62)
-    var sum = numbers[2]+numbers[5]
-    println(sum)
+
+fun add(){
+        var numbers= arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
+        var sum= numbers[1]+numbers[4]
+        println(sum)
+
     println(numbers.indexOf(158))
-    var add = numbers.sortedArray()
-    println(add.contentToString())
 
+    var x =numbers.sortedArray()
+    println(x.contentToString())
+    }
+fun names(name:Array<String>):Array<String>{
+    return name
 
-
-}
-fun secondNames(): Array<String> {
-    var nouns = arrayOf("Ismail", "Iddi", "Awino")
-    return nouns
 }
